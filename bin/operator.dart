98540bf2d7f801c1568f03b346e4accc26e35824
marufjamil777@gmail.com <128314952+marufjamil333@users.noreply.github.com>
2024-02-17 22:59:11 +0600

    changed

---
+++ bin/operator.dart
@@ -0,0 +1,24 @@
+main(){
+
+  int a = 23+45;
+  int b = 23 -4;
+  int c = 454*3;
+  double d = 54/2;
+  int e = 23%2;
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(e);
+
+a++;
+a--;
+++a;
+--a;
+a+=10;
+  print(a++);
+  print(++a);
+  print(a--);
+  print(--a);
+print(a+=10);
+}
